078846ee53375393f9ae3b5106c97a042f449338
Luiza Oliveira <luiza8.marlene@gmai.com>
2022-11-13 17:55:15 -0300

    Estilizando o gráfico com contraste

--- lineChart/src/telas/Home/index.js
+++ lineChart/src/telas/Home/index.js
@@ -1,45 +1,105 @@
 import React from "react";
-import { StyleSheet, View, Text} from "react-native";
-import { VictoryLine, VictoryChart, VictoryTheme } from "victory-native";
+import {Text, Dimensions } from "react-native";
+import { VictoryLine, VictoryChart, VictoryTooltip, VictoryContainer, VictoryAxis } from "victory-native";
+import * as S from "./styles";
 
+const { width, height } = Dimensions.get('window')
+console.log(width)
 const data = [
-    { x: 1, y: 2 },
-    { x: 2, y: 3 },
-    { x: 3, y: 5 },
-    { x: 4, y: 4 },
-    { x: 5, y: 7 }
+    { x: 'Janeiro', y: 20 },
+    { x: 'Fevereiro', y: 19 },
+    { x: 'Março', y: 17 },
+    { x: 'Abril', y: 19 },
+    { x: 'Maio', y: 16 },
+    { x: 'Junho', y: 20 },
+    { x: 'Julho', y: 27 },
+    { x: 'Agosto', y: 28 },
+    { x: 'Setembro', y: 30 },
+    { x: 'Outubro', y: 36 },
+    { x: 'Dezembro', y: 35 }
+];
+const data1 = [
+    { x: 'Janeiro', y: 10 },
+    { x: 'Fevereiro', y: 10 },
+    { x: 'Março', y: 10 },
+    { x: 'Abril', y: 19 },
+    { x: 'Maio', y: 16 },
+    { x: 'Junho', y: 20 },
+    { x: 'Julho', y: 27 },
+    { x: 'Agosto', y: 28 },
+    { x: 'Setembro', y: 30 },
+    { x: 'Outubro', y: 36 },
+    { x: 'Dezembro', y: 35 }
 ];
 
 export default function Home() {
 
     return (
-        <View style={styles.container}>
-            <Text>Livros</Text>
+        <VictoryContainer
+            style={{ backgroundColor: "#000" }}
+            width={width}
+            height={height}
+        >
             <VictoryChart
-                theme={VictoryTheme.material}
+                horizontal={false}
+                style={{
+                    axis: { stroke: '#fff', strokeWidth: 5, color: 'red' },
+                    ticks: { stroke: '#fff', strokeWidth: 5 },
+                    tickLabels: {
+                        fill: 'red',
+                    }
+                }}
             >
-                <VictoryLine
-                    horizontal={true}
+                <VictoryAxis
+                    scale="time"
+                    standalone={false}
+                    tickValues={data}    
                     style={{
-                        data: { stroke: "#c43a31" },
-                        parent: { border: "1px solid #ccc" },
-                        labels: {
-                            fill: '#red',
-                        }
+                        axis: { stroke: '#fff', strokeWidth: 2 },
+                        ticks: { stroke: '#fff', strokeWidth: 1 },
+                        tickLabels: {
+                            fill: '#fff',
+                        },
                     }}
-                    data={data}
                 />
+                <VictoryAxis dependentAxis
+                    domain={[10, 40]}
+                    offsetX={45}
+                    orientation="left"
+                    standalone={false}
+                    style={{
+                        axis: { stroke: '#fff', strokeWidth: 2 },
+                        ticks: { stroke: '#fff', strokeWidth: 1 },
+                        tickLabels: {
+                            fill: '#fff',
+                        },
+                    }}
+                    
+                />
+                <VictoryLine
+                    labelComponent={<VictoryTooltip />}
+                    animate={{ easing: "bounce" }}
+                    data={data}
+                    style={{
+                        data: { stroke: "green", strokeWidth: 4 }
+                    }}
+
+                />
+                {/* <VictoryLine
+                    labelComponent={<VictoryTooltip />}
+                    padding={20}
+                    
+                    horizontal={false}
+                    animate={{ easing: "bounce" }}
+                    style={{
+                        data: { stroke: "red" },
+                        parent: { border: "1px solid #fff" },
+                         margin: '10',
+                    }}
+                    data={data1}
+                /> */}
             </VictoryChart>
-        </View>
+        </VictoryContainer>
     );
 
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#f5fcff"
-    }
-});
---
+++ lineChart/src/telas/Home/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components/native";
+import { VictoryContainer } from "victory-native";
+
+
+export const Container = styled(VictoryContainer)`
+    align-items: center;
+    background-color: #000;
+`
+export const Text = styled.Text`
+    color: #000;
+    font-weight: 400;
+    font-size: 25px;
+    padding-top: 10px;
+    margin: 10px 10px;
+
+`
